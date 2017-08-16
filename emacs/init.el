2c47909e300e970c405e2284a9d090bf28be8dfe
renf10 <dev.renf10@gmail.com>
2017-08-16 19:44:17 +0900

    add:emacs dir, git dir and gitignore

---
+++ emacs/init.el
@@ -0,0 +1,41 @@
+;;theme
+(load-theme 'tango-dark t)
+
+;; スタートアップメッセージを表示させない
+(setq inhibit-startup-message t)
+
+;; バックアップファイルを作成させない
+(setq make-backup-files nil)
+
+;; 終了時にオートセーブファイルを削除する
+(setq delete-auto-save-files t)
+
+;; ウィンドウを透明にする
+;; アクティブウィンドウ／非アクティブウィンドウ（alphaの値で透明度を指定）
+(add-to-list 'default-frame-alist '(alpha . (0.90 0.90)))
+
+;; メニューバーを消す
+(menu-bar-mode -1)
+
+;; ツールバーを消す
+(tool-bar-mode -1)
+
+;; 行数を表示する
+(global-linum-mode t)
+
+;; 対応する括弧を光らせる
+(show-paren-mode 1)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default default default italic underline success warning error])
+ '(custom-enabled-themes (quote (misterioso))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
---
+++ git/git-bootstrap.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+git config --global user.name "First-name Family-name"
+git config --global user.email "username@example.com"
+git config --global core.editor 'vim -c "set fenc=utf-8"'
+git config --global color.diff auto
+git config --global color.status auto
+git config --global color.branch auto
+#git config --global push.default simple
+git config --global core.precomposeunicode true
+git config --global core.quotepath false
